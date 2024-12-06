84b8bdf0652916e5732d13139a1692907b5db416
rimon19 <mominur.hossain@outlook.com>
2024-12-07 00:30:11 +0600

    first commit

---
+++ BlazorInMvc/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using BlazorInMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BlazorInMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string component = null, bool isPartial = false)
+        {
+            ViewData["Component"] = component;
+
+            if (isPartial)
+            {
+                return PartialView("Index");
+            }
+
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BlazorInMvc/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Components.Server;
+
+var builder = WebApplication.CreateBuilder(args);
+// Enable detailed exceptions for development
+builder.Services.Configure<CircuitOptions>(options =>
+{
+    options.DetailedErrors = true;
+});
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages(); // Add this line to register Razor Pages services
+builder.Services.AddServerSideBlazor();
+ 
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+// Map Razor Pages
+app.MapRazorPages(); // Add this line to enable Razor Pages routing
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
+// Map Blazor Hub for server-side Blazor
+app.MapBlazorHub();
+
+// Map fallback to the _Host.cshtml for Blazor
+app.MapFallbackToPage("/_Host");
+// Map routes for MVC controllers
+
+
+
+
+app.Run();
