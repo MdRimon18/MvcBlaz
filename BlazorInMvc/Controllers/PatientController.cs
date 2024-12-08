1afef8049c1098985333fa34d270458fd34f27b4
rimon19 <mominur.hossain@outlook.com>
2024-12-08 23:47:25 +0600

    update

--- BlazorInMvc/Controllers/PatientController.cs
+++ BlazorInMvc/Controllers/PatientController.cs
@@ -6,12 +6,7 @@ namespace DocTimes.Controllers
     public class PatientController : Controller
     {
         public IActionResult Index(bool isPartial = false)
-        {
-            // If the request is an AJAX request, return the partial view only
-            //if (HttpContext.Request.Headers["X-Requested-With"] == "XMLHttpRequest")
-            //{
-            //    return PartialView("Index");
-            //}
+        { 
             if (isPartial)
             {
                 return PartialView("Index");
