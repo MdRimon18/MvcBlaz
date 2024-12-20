f2ae81a2cd7246df2af7bc93a6c9a70b9dc810c2
rimon19 <mominur.hossain@outlook.com>
2024-12-20 23:02:23 +0600

    update

---
+++ BlazorInMvc/Controllers/Mvc/Ecommerce/EcomProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorInMvc.Controllers.Mvc.Ecommerce
+{
+    public class EcomProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
---
+++ BlazorInMvc/Controllers/Mvc/Ecommerce/ItemCartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorInMvc.Controllers.Mvc.Ecommerce
+{
+    public class ItemCartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
